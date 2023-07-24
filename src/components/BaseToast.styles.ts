ce8465f2c222556377a2378a87247411906ab212
David Gómez <davidgggomez0421@gmail.com>
2023-07-24 15:40:55 -0500

    correccion de tamaño del toast dinamico

--- src/components/BaseToast.styles.ts
+++ src/components/BaseToast.styles.ts
@@ -1,13 +1,11 @@
 import { StyleSheet } from 'react-native';
 
-export const HEIGHT = 60;
 export const WIDTH = 340;
 export const BORDER_RADIUS = 6;
 
 export const styles = StyleSheet.create({
   base: {
     flexDirection: 'row',
-    height: HEIGHT,
     width: WIDTH,
     borderRadius: BORDER_RADIUS,
     shadowOffset: { width: 0, height: 0 },
@@ -24,18 +22,18 @@ export const styles = StyleSheet.create({
     paddingHorizontal: 25,
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'flex-start' // In case of RTL, the text will start from the right
+    alignItems: 'flex-start'
   },
   text1: {
     fontSize: 12,
     fontWeight: 'bold',
     marginBottom: 2,
     color: '#000',
-    width: '100%' // Fixes: https://github.com/calintamas/react-native-toast-message/issues/130
+    width: '100%'
   },
   text2: {
     fontSize: 10,
     color: '#979797',
-    width: '100%' // Fixes: https://github.com/calintamas/react-native-toast-message/issues/130
+    width: '100%'
   }
 });
